c9090b4557aecb38bb2eb431aae6aefe8c19ccee
Gustavo Lecca <gustavo.lecca04@gmail.com>
2024-11-27 00:46:23 -0500

    waza

---
+++ CulebriticaPanzona/src/main/java/uni/demo/CulebriticaPanzonaApplication.java
@@ -0,0 +1,13 @@
+package uni.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulebriticaPanzonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulebriticaPanzonaApplication.class, args);
+	}
+
+}
---
+++ CulebriticaPanzona/src/test/java/uni/demo/CulebriticaPanzonaApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulebriticaPanzonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
